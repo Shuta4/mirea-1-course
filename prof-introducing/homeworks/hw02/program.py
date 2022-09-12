5fc9b1d1f93fef0f258e15f1df93e3ca9c4eec1a
Shuta4 <skirdin.nik@gmail.com>
2022-09-12 10:26:16 +0300

    Add ranked graph for prof-intro

---
+++ prof-introducing/homeworks/hw02/program.py
@@ -0,0 +1,53 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import datetime as dt
+from math import log
+
+
+def download_data(code, years):
+    ts_end = int(dt.datetime.now().timestamp())
+    ts_start = ts_end - years * 365 * 86400
+
+    data = pd.read_csv(f"https://query1.finance.yahoo.com/v7/finance/download/{code}?period1={ts_start}&period2={ts_end}&interval=1d&events=history&includeAdjustedClose=true", sep=',')
+    data = data.drop('Volume', axis='columns')
+    data = data.dropna()
+    data['Date'] = data['Date'].apply(lambda x: pd.to_datetime(x))
+    return data
+
+
+def make_plot(data, func, title):
+    plt.figure()
+    plt.plot(data['Date'], data['Close'].apply(lambda x: func(x)))
+    plt.xlabel('Years')
+    plt.ylabel('Value')
+    plt.title(title)
+
+
+def main():
+    code = input('Code >> ')
+    years = int(input('Years >> '))
+
+    print('Downloading data...')
+    data = download_data(code, years)
+    max_value = data['Close'].max()
+    min_value = data['Close'].min()
+
+    print('Creating plots...')
+    plt.style.use('seaborn-whitegrid')
+    make_plot(data, lambda x: x, "$")
+    make_plot(data, lambda x: log(x), "ln$")
+    make_plot(data, lambda x: log(x / max_value), "ln(\$/\$max)")
+    make_plot(data, lambda x: log(x / min_value), "ln(\$/\$min)")
+
+    # Rank values
+    plt.figure()
+    plt.plot(data['Close'].rank(), data['Close'])
+    plt.xlabel('Rank')
+    plt.ylabel('Value')
+    plt.title('Ranked')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
